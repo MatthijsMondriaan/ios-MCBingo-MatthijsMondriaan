61b1f53944f7303651d4cceaba6a601c924f3259
Matthijs <vanderlindendev@gmail.com>
2021-01-31 13:31:42 +0100

    Added BingoScreenCollectionViewCell

---
+++ MCBingo/BingoScreenViewController/BingoScreenCollectionViewCell/BingoScreenCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BingoScreenCollectionViewCell.swift
+//  MCBingo
+//
+//  Created by Matthijs van der Linden on 31/01/2021.
+//
+
+import UIKit
+
+class BingoScreenCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        backgroundColor = .red
+    }
+
+}
