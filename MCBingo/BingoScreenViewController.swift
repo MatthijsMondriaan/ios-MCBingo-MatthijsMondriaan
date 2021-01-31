1937f04a382d557316619c5c97b17849510c7bc4
Matthijs <vanderlindendev@gmail.com>
2021-01-31 13:13:07 +0100

    Added BingoScreenViewController

---
+++ MCBingo/BingoScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BingoScreenViewController.swift
+//  MCBingo
+//
+//  Created by Matthijs van der Linden on 31/01/2021.
+//
+
+import UIKit
+
+class BingoScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.blue;
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
